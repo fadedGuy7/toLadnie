e711fc7536ee007444a9165b8133eb7655061dd0
fadedguy7 <kamkraw91@gmail.com>
2020-01-29 19:41:06 +0100

    Updates

---
+++ precache-manifest.9013a4a41fa02d5e2201b3cca057c4d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41d0b0ea7ec0098af21a4d80a314172",
+    "url": "/toLadnie/index.html"
+  },
+  {
+    "revision": "5c18e9b4a7c2d24076ad",
+    "url": "/toLadnie/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "9fd12b0d425c7c49b9e3",
+    "url": "/toLadnie/static/css/main.6c27b8a2.chunk.css"
+  },
+  {
+    "revision": "5c18e9b4a7c2d24076ad",
+    "url": "/toLadnie/static/js/2.b4c875d5.chunk.js"
+  },
+  {
+    "revision": "7b609b9c1a38d487ee5c17a181d7548e",
+    "url": "/toLadnie/static/js/2.b4c875d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fd12b0d425c7c49b9e3",
+    "url": "/toLadnie/static/js/main.3f800739.chunk.js"
+  },
+  {
+    "revision": "5d806cbc98bf5c7f1e23",
+    "url": "/toLadnie/static/js/runtime-main.e1ba3e9f.js"
+  }
+]);
